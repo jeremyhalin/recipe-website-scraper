fe728481a2798c3a9c546ea05295248f7f424f28
Jérémy Halin <jeremy.halin@gmail.com>
2020-03-31 15:58:00 +0200

    test: scrap from marmiton website

---
+++ test/scrap.test.js
@@ -0,0 +1,13 @@
+import scrapRecipe from '../index';
+
+const marmitonRecipeUrl = "https://www.marmiton.org/recettes/recette_buche-roulee-mangue-citron-vert-au-nutella_382802.aspx";
+
+test("Get recipe from Marmiton", async () => {
+    const recipe = await scrapRecipe(marmitonRecipeUrl);
+    expect(recipe.url).toBe(marmitonRecipeUrl);
+    expect(recipe.title).toBe("Bûche roulée mangue citron vert au Nutella®");
+    expect(recipe.peoples).toBe(6);
+    expect(recipe.preparation_time).toBe(40);
+    expect(recipe.baking_time).toBe(10);
+})
+
